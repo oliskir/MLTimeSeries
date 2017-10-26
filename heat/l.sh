ed66a238ca699deac6f187a629375826d937f880
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2017-10-26 20:43:23 +0200

    gitignore file

--- heat/l.sh
+++ heat/l.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-python main.py -n 4 -e 1000 -v 0 -V -C
-python main.py -n 16 -e 1000 -v 0 -V -C
-python main.py -n 64 -e 1000 -v 0 -V -C
-python main.py -n 128 -e 1000 -v 0 -V -C
+#python main.py -n 4 -e 1000 -v 0 -V -C
+#python main.py -n 16 -e 1000 -v 0 -V -C
+#python main.py -n 64 -e 1000 -v 0 -V -C
+#python main.py -n 128 -e 1000 -v 0 -V -C
 
+python main.py -n 16 -e 10000 -l 1 -f 32 -v 0 -V -C
+python main.py -n 32 -e 10000 -l 1 -f 32 -v 0 -V -C
+python main.py -n 64 -e 10000 -l 1 -f 32 -v 0 -V -C

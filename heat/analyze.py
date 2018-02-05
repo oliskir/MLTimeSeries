09675344e991ea5e832d182d6a33fa9658866ea0
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2018-02-05 14:20:20 +0100

    more analysis

--- heat/analyze.py
+++ heat/analyze.py
@@ -7,22 +7,36 @@ inputfile = '../../heat_load_data/heat_load_weather_calendar.csv'
 
 seed = 7
 
+scaler = 'Standard' #'MinMax'
+
+validate = True
+
 ignore = ['Tout','vWind','sunRad','weekend','observance','national_holiday','school_holiday','hour','weekday','month']
 
 ignore.remove('Tout')
 ignore.remove('vWind')
 ignore.remove('sunRad')
+
+ignore.remove('weekend')
+ignore.remove('weekday')
+ignore.remove('month')
+#ignore.remove('hour')
+
+#ignore.remove('observance')
+#ignore.remove('national_holiday')
+#ignore.remove('school_holiday')
+
              
-e=5000
-for n in [10,20,40]:
+e=20
+for n in [110]:
     logfile = open('analyze.log', 'a')
     logfile.write('\n')
     y = list()
     for i in range(1):
-        yy = train.train(inputfile, 158, 24, 10, 1, [n,n], e, 365, 5, 'Standard', False, False, 0, seed, ignore)
+        yy = train.train(inputfile, 168, 24, 10, 1, [n], e, 365, 5, scaler, validate, False, 0, seed, ignore)
         y.append(yy)
     rmse = sum(y)/len(y)
-    x = 'n=[%i,%i] %.1f' % (n, rmse)
+    x = 'n=[%i] %.1f' % (n, rmse)
     logfile.write(x + '\n')
     logfile.close()
 

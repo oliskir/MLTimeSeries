db614d5ea2b3ea6662ecd6242530cf02ac56ec64
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2018-02-19 10:42:13 -0400

    analyze outputs average test result

--- analyze.py
+++ analyze.py
@@ -19,31 +19,34 @@ ignore.remove('Tout')
 ignore.remove('vWind')
 ignore.remove('sunRad')
 
-ignore.remove('weekend')
+#ignore.remove('weekend')
 #ignore.remove('weekday')
 #ignore.remove('month')
 #ignore.remove('hour')
 
-#ignore.remove('observance')
-#ignore.remove('national_holiday')
-#ignore.remove('school_holiday')
+ignore.remove('observance')
+ignore.remove('national_holiday')
+ignore.remove('school_holiday')
 
              
-n=100
-for e in [5,15,45]:
+n=110
+for e in [15]:
     logfile = open('analyze.log', 'a')
     logfile.write('\n')
     y = list()
-    for i in range(1):
+    z = list()
+    for i in range(5):
         yy, lfile, mfile, wfile, sfile = train.train(inputfile, 168, 24, 10, 1, [n], e, days, 5, scaler, validate, False, 0, seed, ignore)
         y.append(yy)
         
-        z = test.test("../heat_load_data/test_data.pkl", lfile, mfile, wfile, sfile)
-        zs = '%.1f, ' % z
+        zz = test.test("../heat_load_data/test_data.pkl", lfile, mfile, wfile, sfile)
+        z.append(zz)
+        zs = '%.1f, ' % zz
         logfile.write(zs)
         
     rmse = sum(y)/len(y)
-    x = '  [%.1f]' % (rmse)
+    rmse_test = sum(z)/len(z)
+    x = '  [%.1f]' % (rmse_test)
     logfile.write(x + '\n')
     logfile.close()
 
